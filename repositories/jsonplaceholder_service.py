b49ba24b87335b895a82e5a5a146edd0e8f098e2
Ana Bergese <violetagele22@gmail.com>
2023-12-23 15:55:32 +0100

    Added fetch_data method to delete duplicated code in repositories/jsonplaceholder_service.py

--- repositories/jsonplaceholder_service.py
+++ repositories/jsonplaceholder_service.py
@@ -2,44 +2,36 @@ import httpx
 
 JSONPLACEHOLDER_API_URL = 'https://jsonplaceholder.typicode.com'
 
-async def get_all_posts():
-    url = f'{JSONPLACEHOLDER_API_URL}/posts'
+async def fetch_data(url):
     async with httpx.AsyncClient() as client:
         response = await client.get(url)
-    return response.json()
+        response.raise_for_status()
+        return response.json()
+
+async def get_all_posts():
+    url = f'{JSONPLACEHOLDER_API_URL}/posts'
+    return await fetch_data(url)
 
 async def get_post_by_id(id):
     url = f'{JSONPLACEHOLDER_API_URL}/posts/{id}'
-    async with httpx.AsyncClient() as client:
-        response = await client.get(url)
-    return response.json()
+    return await fetch_data(url)
 
 async def get_all_comments():
     url = f'{JSONPLACEHOLDER_API_URL}/comments'
-    async with httpx.AsyncClient() as client:
-        response = await client.get(url)
-    return response.json()
+    return await fetch_data(url)
 
 async def get_comment_by_id(id):
     url = f'{JSONPLACEHOLDER_API_URL}/comments/{id}'
-    async with httpx.AsyncClient() as client:
-        response = await client.get(url)
-    return response.json()
+    return await fetch_data(url)
 
 async def get_comments_by_post_id(post_id):
     url = f'{JSONPLACEHOLDER_API_URL}/comments?postId={post_id}'
-    async with httpx.AsyncClient() as client:
-        response = await client.get(url)
-    return response.json()
+    return await fetch_data(url)
 
 async def get_all_users():
     url = f'{JSONPLACEHOLDER_API_URL}/users'
-    async with httpx.AsyncClient() as client:
-        response = await client.get(url)
-    return response.json()
+    return await fetch_data(url)
 
 async def get_user_by_id(id):
     url = f'{JSONPLACEHOLDER_API_URL}/users/{id}'
-    async with httpx.AsyncClient() as client:
-        response = await client.get(url)
-    return response.json()
+    return await fetch_data(url)
