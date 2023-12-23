c696d9f157af47d18c9a431910ead6ccef89ca99
Ana Bergese <violetagele22@gmail.com>
2023-12-23 19:24:59 +0100

    in main.py used HTTPException and implement try-except-finally to manage errors

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import Depends, FastAPI, HTTPException
+from fastapi.responses import JSONResponse
 from repositories.jsonplaceholder_repository import *
 from models.model import Post, User
 
@@ -19,30 +20,41 @@ async def get_post_with_comments(
     post_repository: PostRepository = Depends(get_post_repository),
     comment_repository: CommentRepository = Depends(get_comment_repository),
 ):
-    post = await post_repository.get_by_id(post_id)
-    if not post:
-        raise HTTPException(status_code=404, detail="Post not found")
-
-    comments = await comment_repository.get_comments_by_post_id(post_id)
-
-    post_with_comments = {
-        "user_id": post["userId"],
-        "id": post["id"],
-        "title": post["title"],
-        "body": post["body"],
-        "comments": comments
-    }
-
-    return post_with_comments
+    try:
+        post = await post_repository.get_by_id(post_id)
+        if post:
+            comments = await comment_repository.get_comments_by_post_id(post_id)
+            post_with_comments = {
+                "user_id": post["userId"],
+                "id": post["id"],
+                "title": post["title"],
+                "body": post["body"],
+                "comments": comments
+            }
+            return JSONResponse(content=post_with_comments)
+        else:
+            raise HTTPException(status_code=404, detail="Post not found")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Internal Server Error: {str(e)}")
 
 @app.get("/posts", response_model=list[Post])
 async def read_all_posts(post_repository: PostRepository = Depends(get_post_repository)):
-    posts = await post_repository.get_all()
-    return posts
-
+    try:
+        posts = await post_repository.get_all()
+        if posts:
+            return JSONResponse(content=posts)
+        else:
+            raise HTTPException(status_code=404, detail="Posts not found")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Internal Server Error: {str(e)}") 
+    
 @app.get("/users/{user_id}", response_model=User)
 async def read_user_by_id(user_id: int, user_repository: UserRepository = Depends(get_user_repository)):
-    user = await user_repository.get_by_id(user_id)
-    if not user:
-        raise HTTPException(status_code=404, detail="User not found")
-    return user
+    try:
+        user = await user_repository.get_by_id(user_id)
+        if user:
+            return JSONResponse(content=user)
+        else:
+            raise HTTPException(status_code=404, detail="User not found")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Internal Server Error: {str(e)}")
