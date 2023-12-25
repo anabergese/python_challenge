37c755df20e4fd41e70997e717a5b52aa12fd757
Ana Bergese <violetagele22@gmail.com>
2023-12-26 00:44:07 +0100

    refactor project tree by creating backend folder and moved everything inside it

---
+++ backend/src/repositories/jsonplaceholder_service.py
@@ -0,0 +1,57 @@
+import httpx
+from backend.src.domain.model import Post
+
+JSONPLACEHOLDER_API_URL = "https://jsonplaceholder.typicode.com"
+
+
+async def fetch_data(url):
+    try:
+        async with httpx.AsyncClient() as client:
+            response = await client.get(url)
+            response.raise_for_status()
+            return response.json()
+    except httpx.RequestError as req_err:
+        print(f"Request error while fetching data from {url}: {req_err}")
+        raise
+    except httpx.HTTPStatusError as http_err:
+        print(
+            f"HTTP error ({http_err.response.status_code}) occurred while fetching data from {url}"
+        )
+        raise
+
+
+async def get_all_posts():
+    url = f"{JSONPLACEHOLDER_API_URL}/posts"
+    return await fetch_data(url)
+
+
+async def get_post_by_id(id):
+    url = f"{JSONPLACEHOLDER_API_URL}/posts/{id}"
+    post_data = await fetch_data(url)
+    post_instance = Post(**post_data) if post_data else None
+    return post_instance
+
+
+async def get_all_comments():
+    url = f"{JSONPLACEHOLDER_API_URL}/comments"
+    return await fetch_data(url)
+
+
+async def get_comment_by_id(id):
+    url = f"{JSONPLACEHOLDER_API_URL}/comments/{id}"
+    return await fetch_data(url)
+
+
+async def get_comments_by_post_id(post_id):
+    url = f"{JSONPLACEHOLDER_API_URL}/comments?postId={post_id}"
+    return await fetch_data(url)
+
+
+async def get_all_users():
+    url = f"{JSONPLACEHOLDER_API_URL}/users"
+    return await fetch_data(url)
+
+
+async def get_user_by_id(id):
+    url = f"{JSONPLACEHOLDER_API_URL}/users/{id}"
+    return await fetch_data(url)
