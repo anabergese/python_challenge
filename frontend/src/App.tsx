084f39f27bfe8f974b024c6da3f74566f8a84662
Ana Bergese <violetagele22@gmail.com>
2024-01-06 11:31:01 +0100

    Rollback all implementations related to test, because there were lot of error related to instalation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -20,7 +20,9 @@ const App = () => {
         <div className="App">
           <section className="hero is-warning mb-4">
             <div className="hero-body">
-              <p className="title">The Blog</p>
+              <p className="title" data-testid="app-title">
+                The Blog
+              </p>
               <p className="subtitle">Code Challenge</p>
             </div>
           </section>
